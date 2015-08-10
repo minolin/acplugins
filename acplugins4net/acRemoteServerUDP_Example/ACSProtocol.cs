363121a00e41c500f986aa840faa79972de1cdd1
never-eat-yellow-snow <never-eat-yellow-snow@users.noreply.github.com>
2015-08-10 18:29:08 +0200

    update with new example code from AC 1.2.3

--- acplugins4net/acRemoteServerUDP_Example/ACSProtocol.cs
+++ acplugins4net/acRemoteServerUDP_Example/ACSProtocol.cs
@@ -13,6 +13,11 @@ namespace acRemoteServerUDP_Example {
         public const byte ACSP_CAR_INFO = 54; // Sent as response to ACSP_GET_CAR_INFO command
         public const byte ACSP_END_SESSION = 55;
         public const byte ACSP_LAP_COMPLETED = 73;
+        public const byte ACSP_VERSION = 56;
+        public const byte ACSP_CHAT = 57;
+        public const byte ACSP_CLIENT_LOADED = 58;
+        public const byte ACSP_SESSION_INFO = 59;
+        public const byte ACSP_ERROR = 60;
 
         // EVENTS
         public const byte ACSP_CLIENT_EVENT = 130;
@@ -25,7 +30,10 @@ namespace acRemoteServerUDP_Example {
         public const byte ACSP_REALTIMEPOS_INTERVAL = 200;
         public const byte ACSP_GET_CAR_INFO = 201;
         public const byte ACSP_SEND_CHAT = 202; // Sends chat to one car
-        public const byte ACSP_BROADCAST_CHAT = 203; // Sends chat to everybody 
+        public const byte ACSP_BROADCAST_CHAT = 203; // Sends chat to everybody
+        public const byte ACSP_GET_SESSION_INFO = 204;
+        public const byte ACSP_SET_SESSION_INFO = 205;
+        public const byte ACSP_KICK_USER = 206;
         
     }
 }
--- acplugins4net/acRemoteServerUDP_Example/Program.cs
+++ acplugins4net/acRemoteServerUDP_Example/Program.cs
@@ -44,6 +44,57 @@ namespace acRemoteServerUDP_Example {
             bw.Write(Encoding.UTF32.GetBytes(message));
         }
 
+        static void testSetSessionInfo(UdpClient client) {
+            // Prepare the packet to send
+            var buffer = new byte[1000];
+            var bw = new BinaryWriter(new MemoryStream(buffer));
+
+            // Packet ID
+            bw.Write(ACSProtocol.ACSP_SET_SESSION_INFO);
+
+            // Session Index we want to change, be very careful with changing the current session tho, some stuff might not work as expected
+            byte session_index=1;
+            bw.Write(session_index);
+
+            // Session name
+            writeStringW(bw,"SuperCoolServer"); // Careful here, the server is still broadcasting ASCII strings to the clients for this
+            
+            // Session type
+            bw.Write((byte)3);
+
+            // Laps
+            bw.Write((UInt32)250);
+
+            // Time (in seconds)
+            bw.Write((UInt32)0);
+
+            // Wait time (in seconds)
+            bw.Write((UInt32)60);
+            
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // IP address of the server, with the port in UDP_PLUGIN_LOCAL_PORT in server_cfg
+
+            client.Send(buffer, (int)bw.BaseStream.Length, ep);
+
+
+        }
+
+        static void testGetSessionInfo(UdpClient client) {
+            // Prepare the packet to send
+            var buffer = new byte[100];
+            var bw = new BinaryWriter(new MemoryStream(buffer));
+
+            // Packet ID
+            bw.Write(ACSProtocol.ACSP_GET_SESSION_INFO);
+
+            // Session index (-1 to request the current session)
+            bw.Write((Int16)(-1));
+                      
+
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // IP address of the server, with the port in UDP_PLUGIN_LOCAL_PORT in server_cfg
+
+            client.Send(buffer, (int)bw.BaseStream.Length, ep);
+        }
+
         static void testGetCarInfo(UdpClient client, byte carID) {
             // Prepare the packet to send
             var buffer = new byte[100];
@@ -89,6 +140,8 @@ namespace acRemoteServerUDP_Example {
             return res;
         }
 
+
+
         public static void sendChat(UdpClient client,byte carid,string message) {
             // Prepare the packet to send
             var buffer = new byte[255];
@@ -127,6 +180,20 @@ namespace acRemoteServerUDP_Example {
 
         }
 
+        public static void testKick(UdpClient client, byte userid) {
+            // Prepare the packet to send
+            var buffer = new byte[255];
+            var bw = new BinaryWriter(new MemoryStream(buffer));
+
+            // Packet ID
+            bw.Write(ACSProtocol.ACSP_KICK_USER);
+            bw.Write(userid);
+                       
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // IP address of the server, with the port in UDP_PLUGIN_LOCAL_PORT in server_cfg
+
+            client.Send(buffer, (int)bw.BaseStream.Length, ep);
+        }
+
         static void Main(string[] args) {
 
             IPEndPoint ep = new IPEndPoint(IPAddress.Any, 12000);
@@ -143,8 +210,47 @@ namespace acRemoteServerUDP_Example {
                 var packet_id = br.ReadByte();
                 Console.WriteLine("PACKET ID:" + packet_id);
                 switch (packet_id) {
+                    case ACSProtocol.ACSP_ERROR: {
+                            var msg = readStringW(br);
+
+                            Console.WriteLine(msg);
+                        }
+                        break;
+                    case ACSProtocol.ACSP_CHAT: {
+                            var car_id = br.ReadByte();
+                            var msg = readStringW(br);
+
+                            Console.WriteLine("CHAT FROM CAR:" + (int)car_id + " MSG:" + msg);
+                        }
+                        break;
+                    case ACSProtocol.ACSP_CLIENT_LOADED: {
+                            var car_id = br.ReadByte();
+
+                            Console.WriteLine("CLIENT LOADED:" + (int)car_id);
+
+                        }
+                        break;
+
+                    case ACSProtocol.ACSP_VERSION:
+                        var protocol_version = br.ReadByte();
+
+                        Console.WriteLine("PROTOCOL VERSION IS:" + (int)protocol_version);
+                        break;
                     case ACSProtocol.ACSP_NEW_SESSION:
                         Console.WriteLine("New session started");
+                        goto case ACSProtocol.ACSP_SESSION_INFO;
+                    case ACSProtocol.ACSP_SESSION_INFO:
+                        Console.WriteLine("Session Info");
+
+                        var version = br.ReadByte(); // UDP Plugin protocol version, in case you miss the first ACSP_VERSION message sent by the server at startup
+                        var sess_index = br.ReadByte(); // The index of the session in the message
+                        var current_session_index = br.ReadByte(); // The index of the current session in the server
+                        var session_count = br.ReadByte(); // The number of sessions in the server
+                        
+                        
+                        var server_name = readStringW(br);
+                        var track = readString(br);
+                        var track_config = readString(br);
                         var name = readString(br);
                         var type = br.ReadByte();
                         var time = br.ReadUInt16();
@@ -153,22 +259,33 @@ namespace acRemoteServerUDP_Example {
                         var ambient_temp = br.ReadByte();
                         var road_temp = br.ReadByte();
                         var weather_graphics = readString(br);
+                        var elapsedMS = br.ReadInt32(); // Milliseconds from the start (this might be negative for races with WaitTime)
 
+                        Console.WriteLine("PROTOCOL VERSION:" + version);
+                        Console.WriteLine("SESSION INDEX:" + sess_index + "/" + session_count + " CURRENT SESSION:" + current_session_index);
+                        Console.WriteLine("SERVER NAME:" + server_name);
+                        Console.WriteLine("TRACK:" + track+" ["+track_config+"]");
                         Console.WriteLine("NAME:"+name);
                         Console.WriteLine("TYPE:" + type);
                         Console.WriteLine("TIME:" + time);
                         Console.WriteLine("LAPS:" + laps);
                         Console.WriteLine("WAIT TIME:" + waitTime);
                         Console.WriteLine("WEATHER:" + weather_graphics + " AMBIENT:" + ambient_temp + " ROAD:" + road_temp);
+                        Console.WriteLine("ELAPSED:" + elapsedMS);
 
-                        // UNCOMMENT to enable realtime position reports
-                        enableRealtimeReport(client);
+                        if (packet_id == ACSProtocol.ACSP_NEW_SESSION) {
+                            // UNCOMMENT to enable realtime position reports
+                            enableRealtimeReport(client);
 
-                        // TEST ACSP_GET_CAR_INFO
-                        testGetCarInfo(client, 2);
+                            // TEST ACSP_GET_CAR_INFO
+                            testGetCarInfo(client, 2);
 
-                        sendChat(client, 0, "CIAO BELLO!");
-                        broadcastChat(client, "E' arrivat' 'o pirit'");
+                            sendChat(client, 0, "CIAO BELLO!");
+                            broadcastChat(client, "E' arrivat' 'o pirit'");
+
+                            // Test Kick User, bad index, it will also trigger an error
+                            testKick(client, 230);
+                        }
                         break;
                     case ACSProtocol.ACSP_END_SESSION:
                         Console.WriteLine("ACSP_END_SESSION");
@@ -220,6 +337,9 @@ namespace acRemoteServerUDP_Example {
 
                         Console.WriteLine("CAR:" + car_id + " " + model + " [" + skin + "] DRIVER:" + driver_name + " TEAM:" + driver_team + " GUID:" + driver_guid+" CONNECTED:"+is_connected);
 
+                        // TEST GET SESSION INFO. This will generate a loop of messages!
+                        // testGetSessionInfo(client);
+                        testSetSessionInfo(client);
                         }
                         break;
                     case ACSProtocol.ACSP_CAR_UPDATE: {
